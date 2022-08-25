1d3087e59d784ced9c20f44206092185735348d7
Tofu <osawa.shuto@gmail.com>
2022-08-25 23:05:07 +0900

    added Read part

--- DataGridComponent/CRUD/Read.cs
+++ DataGridComponent/CRUD/Read.cs
@@ -1,10 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
 namespace DataGridComponent
 {
     public class Read
     {
-        public void ReadRow()
+        public static void ReadRow(STBInfo stbInfo, Form form)
         {
-            
+            foreach (Control c in form.Controls)
+            {
+                if (c.Name == "nameTextBox")
+                {
+                    c.Text = stbInfo.Name;
+                }
+                else if (c.Name == "ipAddressTextBox")
+                {
+                    c.Text = stbInfo.IPAddress;
+                }
+            }
         }
     }
 }
--- DataGridComponent/Form1.cs
+++ DataGridComponent/Form1.cs
@@ -21,48 +21,76 @@ namespace DataGridComponent
         DataGridView dataGridView = new DataGridView();
         private TextBox nameTextBox;
         private Label nameLabel;
+        private Label ipAddressLabel;
         private TextBox ipAddressTextBox;
         private Button createRowButton;
         private Button deleteRowButton;
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Controls.Add(dataGridView);
-            nameLabel = Components.LabelComponent("Name",300, 0); 
+            dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView_CellClicked);
+
+            nameLabel = Components.LabelComponent("Name",300, 0);
+            nameLabel.Name = "nameLabel";
             this.Controls.Add(nameLabel);
+
             nameTextBox = Components.TextBoxComponent(300, 100);
+            nameTextBox.Name = "nameTextBox";
             this.Controls.Add(nameTextBox);
-            nameLabel = Components.LabelComponent("IP Address",400, 0); 
-            this.Controls.Add(nameLabel);
+
+            ipAddressLabel = Components.LabelComponent("IP Address",400, 0);
+            ipAddressLabel.Name = "ipAddressLabel";
+            this.Controls.Add(ipAddressLabel);
+
             ipAddressTextBox = Components.TextBoxComponent(400, 100);
+            ipAddressTextBox.Name = "ipAddressTextBox";
             this.Controls.Add(ipAddressTextBox);
+
             createRowButton = Components.ButtonComponent("Create",300,300,this,Button_Clicked_Create);
+            createRowButton.Name = "createRowButton";
             this.Controls.Add(createRowButton);
+
             deleteRowButton = Components.ButtonComponent("Delete",400,300,this,Button_Clicked_Delete);
+            deleteRowButton.Name = "deleteRowButton";
             this.Controls.Add(deleteRowButton);
         }
-        
+
+        private void DataGridView_CellClicked(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dataGridView.CurrentCell.RowIndex;
+            Read.ReadRow(stbList[index], this);
+        }
+
         private void Button_Clicked_Delete(object sender, EventArgs e)
         {
             if (stbList.Count > 0)
             {
                 stbList = Delete.DeleteRow(dataGridView.CurrentCell.RowIndex, stbList);
-                dataGridView.DataSource = null;
-                dataGridView.DataSource = stbList;
-                foreach (TextBox tb in this.Controls.OfType<TextBox>())
-                {
-                    tb.Text = String.Empty;
-                }
+                UpdateDataGridView(stbList);
+                UpdateTextBoxes();
             }
         }
+        
         private void Button_Clicked_Create(object sender, EventArgs e)
         {
             stbList = Create.CreateRow(new STBInfo(nameTextBox.Text,ipAddressTextBox.Text),stbList);
+            UpdateDataGridView(stbList);
+            UpdateTextBoxes();
+        }
+        public void UpdateDataGridView(List<STBInfo> stbList)
+        {
             dataGridView.DataSource = null;
             dataGridView.DataSource = stbList;
+        }
+
+        public void UpdateTextBoxes()
+        {
             foreach ( TextBox tb in this.Controls.OfType<TextBox>()) {
-               tb.Text = String.Empty; 
+                tb.Text = String.Empty; 
             } 
         }
+
+      
     }
 }
 
