8ee8cd51bc06a05171f2c1339578c014337f551c
Tofu <osawa.shuto@gmail.com>
2022-08-25 22:36:47 +0900

    added some tests

---
+++ DataGridComponentTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Windows.Forms;
+using DataGridComponent;
+using NUnit.Framework;
+
+namespace DataGridComponentTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+
+    }
+}
